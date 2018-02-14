bb036e11ff4ebed3062f10767d848307adfaa751
javierfajnolla <javierfajnolla@gmail.com>
2018-02-14 11:05:59 -0300

    Para terminar: - He comentado parte del código que es de la forma antigua, muy poco eficiente
    - He dejado limpia la parte que funciona usando el do.call y la lista de rásteres

--- mosaic_rasters.R
+++ mosaic_rasters.R
@@ -1,29 +1,8 @@
 # Unir los tiles de cada tipo (tree cover y forest loss)
-
 library(tidyverse)
 library(raster)
 
 # Tree cover
-cat('\n', "Tree cover")
-files_tc <- list.files("data/raster/tc")
-
-cat('\n', "1")
-tc_world <- raster(paste0("data/raster/tc/", files_tc[1]))
-for (x in 2:length(files_tc)){
-  cat(" ", x)
-  tc_r <- raster(paste0("data/raster/tc/", files_tc[x]))
-  tc_world <- mosaic(tc_world, tc_r, fun = mean)
-}
-
-cat('\n', "Saving")
-dir.create("data/raster/world", showWarnings = F)
-dir.create("data/raster/world/tc", showWarnings = F)
-writeRaster(tc_world, "data/raster/world/tc/treecover2000", format ="GTiff")
-
-rm(tc_world); rm(tc_r)
-gc()
-
-###########
 # NW
 files_tc <- list.files("data/raster/tc", full.names=TRUE, recursive=TRUE)
 files_tc_N <- files_tc[grepl("N_", files_tc)]
@@ -62,10 +41,55 @@ rast_mosaic_SE <- do.call(mosaic, rast_list_SE)
 # World
 rast_mosaic <- mosaic(rast_mosaic_NW, rast_mosaic_NE, rast_mosaic_SW, rast_mosaic_SE, fun = mean)
 
-rast_mosaic %>% plot
-###########
+# rast_mosaic %>% plot
+
+writeRaster(rast_mosaic, "data/raster/world/tc/treecover2000", format = "GTiff")
+rm(rast_mosaic_NE, rast_mosaic_NW, rast_mosaic_SE, rast_mosaic_SW)
+rm(rast.mosaic, rast.mosaic_NW)
 
 
+# Forest Loss
+files_fl <- list.files("data/raster/fl", full.names=TRUE, recursive=TRUE)
+rast_list_fl <- lapply(1:length(files_fl),
+                       function(x) {
+                         raster(files_fl[x])
+                       })
+rast_list_fl$fun <- mean
+rast_mosaic_fl <- do.call(mosaic, rast_list_fl)
+writeRaster(rast_mosaic_fl, "data/raster/world/tc/forestloss_2000_2016", format = "GTiff")
+
+# Forest Loss
+files_fl2010 <- list.files("data/raster/fl2010", full.names=TRUE, recursive=TRUE)
+rast_list_fl2010 <- lapply(1:length(files_fl2010),
+                       function(x) {
+                         raster(files_fl2010[x])
+                       })
+rast_list_fl2010$fun <- mean
+rast_mosaic_fl2010 <- do.call(mosaic, rast_list_fl2010)
+writeRaster(rast_mosaic_fl2010, "data/raster/world/tc/forestloss_2000_2010", format = "GTiff")
+
+
+#### (old way)
+
+# Tree cover
+cat('\n', "Tree cover")
+files_tc <- list.files("data/raster/tc")
+
+cat('\n', "1")
+tc_world <- raster(paste0("data/raster/tc/", files_tc[1]))
+for (x in 2:length(files_tc)){
+  cat(" ", x)
+  tc_r <- raster(paste0("data/raster/tc/", files_tc[x]))
+  tc_world <- mosaic(tc_world, tc_r, fun = mean)
+}
+
+cat('\n', "Saving")
+dir.create("data/raster/world", showWarnings = F)
+dir.create("data/raster/world/tc", showWarnings = F)
+writeRaster(tc_world, "data/raster/world/tc/treecover2000", format ="GTiff")
+
+rm(tc_world); rm(tc_r)
+gc()
 
 # Forest Loss
 cat('\n', "Forest Loss")
@@ -106,13 +130,3 @@ writeRaster(fl2010_world, "data/raster/world/fl2010/forestloss_2000_2010", forma
 cat('\n', "Saving")
 rm(tc_world); rm(tc_r)
 gc()
-
-
-files_tc <- list.files("data/raster/tfl2000", full.names=TRUE, recursive=TRUE)
-rast.list <- lapply(1:length(files_tc),
-                    function(x) {
-                      raster(files_tc[x])
-                    })
-rast.list$fun <- mean
-rast.mosaic <- do.call(mosaic,rast.list)
-rast.mosaic %>% plot
